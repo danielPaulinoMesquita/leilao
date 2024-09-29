44db42dca8543ad9a611119d64b3614c466fd777
Daniel Paulino <daniel-paulino@hotmail.com>
2024-09-29 00:40:19 -0300

    4- Criando Entities Lances (bids) and Repository for them

---
+++ internal/infra/database/bid/create_bid.go
@@ -0,0 +1,67 @@
+package bid
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"leilao/configuration/logger"
+	"leilao/internal/entity/auction_entity"
+	"leilao/internal/entity/bid_entity"
+	"leilao/internal/infra/database/auction"
+	"leilao/internal/internal_error"
+	"sync"
+)
+
+type BidEntityMongo struct {
+	Id        string  `bson:"id"`
+	UserId    string  `bson:"user_id"`
+	AuctionId string  `bson:"auction_id"`
+	Amount    float64 `bson:"amount"`
+	Timestamp int64   `bson:"timestamp"`
+}
+
+type BidRepository struct {
+	Collection        *mongo.Collection
+	AuctionRepository *auction.AuctionRepository
+}
+
+func (bd *BidRepository) CreateBid(ctx context.Context, bidEntities []bid_entity.Bid) *internal_error.InternalError {
+	var wq sync.WaitGroup
+
+	for _, bid := range bidEntities {
+
+		wq.Add(1)
+
+		go func(bidValue bid_entity.Bid) {
+			defer wq.Done()
+
+			auctionEnity, err := bd.AuctionRepository.FindAuctionById(ctx, bidValue.AuctionId)
+			if err != nil {
+				logger.Error("Error trying to find auction by id", err)
+				return
+			}
+
+			if auctionEnity.Status != auction_entity.Active {
+				return
+			}
+
+			bidEntityMongo := &BidEntityMongo{
+				Id:        bidValue.Id,
+				UserId:    bidValue.UserId,
+				AuctionId: bidValue.AuctionId,
+				Amount:    bidValue.Amount,
+				Timestamp: bidValue.Timestamp.Unix(),
+			}
+
+			if _, err := bd.Collection.InsertOne(ctx, bidEntityMongo); err != nil {
+				logger.Error("Error trying to insert bid", err)
+				return
+			}
+
+		}(bid)
+
+	}
+
+	wq.Wait()
+	return nil
+
+}
---
+++ internal/infra/database/bid/find_bid.go
@@ -0,0 +1,67 @@
+package bid
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"leilao/configuration/logger"
+	"leilao/internal/entity/bid_entity"
+	"leilao/internal/internal_error"
+	"time"
+)
+
+func (bd *BidRepository) FindBidByAuctionId(
+	auctionId string, ctx context.Context) ([]bid_entity.Bid, *internal_error.InternalError) {
+	filter := bson.M{"auction_id": auctionId}
+
+	cursor, err := bd.Collection.Find(ctx, filter)
+	if err != nil {
+		logger.Error(fmt.Sprintf("Error trying to find bids by auction %s", auctionId), err)
+		return nil, internal_error.NewInternalServerError(
+			fmt.Sprintf("Error trying to find bids by auction %s", auctionId))
+	}
+
+	var bidEntityMongo []BidEntityMongo
+	if err = cursor.All(ctx, &bidEntityMongo); err != nil {
+		logger.Error(fmt.Sprintf("Error trying to find bids by auction %s", auctionId), err)
+		return nil, internal_error.NewInternalServerError(
+			fmt.Sprintf("Error trying to find bids by auction %s", auctionId))
+
+	}
+
+	var bidEntites []bid_entity.Bid
+	for _, b := range bidEntityMongo {
+		bidEntites = append(bidEntites, bid_entity.Bid{
+			Id:        b.Id,
+			UserId:    b.UserId,
+			AuctionId: b.AuctionId,
+			Amount:    b.Amount,
+			Timestamp: time.Unix(int64(b.Timestamp), 0),
+		})
+	}
+
+	return bidEntites, nil
+}
+
+func (bd *BidRepository) FindWinningBidByAuctionId(
+	ctx context.Context,
+	auctionId string) (*bid_entity.Bid, *internal_error.InternalError) {
+	filter := bson.M{"auction_id": auctionId}
+
+	var bidEntityMongo BidEntityMongo
+	opts := options.FindOne().SetSort(bson.D{{"amount", -1}})
+	if err := bd.Collection.FindOne(ctx, filter, opts).Decode(&bidEntityMongo); err != nil {
+		logger.Error("Error trying to find the auction winner", err)
+		return nil, internal_error.NewInternalServerError("Error trying to find the auction winner")
+	}
+
+	return &bid_entity.Bid{
+		Id:        bidEntityMongo.Id,
+		UserId:    bidEntityMongo.UserId,
+		AuctionId: bidEntityMongo.AuctionId,
+		Amount:    bidEntityMongo.Amount,
+		Timestamp: time.Unix(bidEntityMongo.Timestamp, 0),
+	}, nil
+
+}
